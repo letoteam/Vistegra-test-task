765db25d239013166bbeede8d2ad5ec9aa2489ff
Vahouski <goastman53@gmail.com>
2022-07-25 19:21:07 +0300

    feat: cart sum

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CartItem as CartItemType } from "../../types/CartItem";
 import CartItem from "./CartItem/CartItem";
 import styles from "./styles.module.css";
@@ -14,20 +14,44 @@ export default function Cart({
   setCartItemAmount,
   deleteItem,
 }: CartProps) {
+  const [sum, setSum] = useState<Number>(0);
+
+  useEffect(() => {
+    const newSum = items.reduce((result, currentItem) => {
+      let price: number = +currentItem.price;
+      if (currentItem.amount > 1) {
+        price *= currentItem.amount;
+      }
+
+      return result + price;
+    }, 0);
+
+    setSum(newSum);
+  }, [items]);
+
+  const renderSum = () => {
+    // @ts-ignore
+    return <p className={styles.sum}>Sum: {sum}</p>;
+  };
+
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <div className={styles.cart}>
       {items.length ? (
-        items.map((item) => (
-          <CartItem
-            key={item.id}
-            item={item}
-            setCartItemAmount={setCartItemAmount}
-            deleteItem={deleteItem}
-          />
-        ))
+        <>
+          {items.map((item) => (
+            <CartItem
+              key={item.id}
+              item={item}
+              setCartItemAmount={setCartItemAmount}
+              deleteItem={deleteItem}
+            />
+          ))}
+
+          {renderSum()}
+        </>
       ) : (
-        <p>No Reservations</p>
+        <p className={styles.noItemsText}>No Items Yet</p>
       )}
     </div>
   );
